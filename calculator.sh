461b2565dd1ceb0780a7c24d180d6b35df23300c
Rithin <venkata9695@gmail.com>
2020-06-05 17:32:54 -0400

    Initial commit

---
+++ calculator.sh
@@ -0,0 +1,24 @@
+echo "Enter Two numbers : "
+read num1 
+read num2 
+
+# Input type of operation 
+echo "Enter Choice :"
+echo "1. Addition"
+echo "2. Subtraction"
+echo "3. Multiplication"
+echo "4. Division"
+read choice 
+
+# Switch Case for calculation 
+case $choice in
+1)res=`echo $num1 + $num2 | bc` 
+;; 
+2)res=`echo $num1 - $num2 | bc` 
+;; 
+3)res=`echo $num1 \* $num2 | bc` 
+;; 
+4)res=`echo "scale=2; $num1 / $num2" | bc` 
+;; 
+esac
+echo "Result : $res"
